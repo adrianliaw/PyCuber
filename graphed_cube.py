c582bc6d42426077ce9f80d97f97557a55c0218c
adrianliaw <adrianliaw2000@gmail.com>
2014-09-21 19:33:13 +0800

    graphed_cube is not efficient

--- graphed_cube.py
+++ graphed_cube.py
@@ -15,6 +15,58 @@ Square.type = "square"
 Cuboid = namedtuple("Cuboid", ["x", "y", "z"])
 Cuboid.type = "cuboid"
 
+from copy import deepcopy
+
+
+
+class _CuboidsFilterDict(dict):
+
+    """
+    Don't use this, it's something messy.
+    """
+
+    def filter(self, key=[], nbr=[]):
+
+        """
+        CubeGraph.filter( 
+            key = [ " its.colour == 'R' " ], 
+            nbr = [ " its.type == 'cuboid' " ]
+        )
+        is same as
+        Cube[{
+            "key" : [ " its.colour == 'R' " ]
+            "nbr" : [ " its.type == 'cuboid' " ]
+        }]
+        """
+
+        query = {"key":key, "nbr":nbr}
+        result = deepcopy(self)
+        deleted = set()
+        for exp in query["key"]:
+            for its in self:
+                if its not in deleted:
+                    try:
+                        if not eval(exp):
+                            deleted.add(its)
+                    except AttributeError:
+                        deleted.add(its)
+        for obj in deleted:
+            del result[obj]
+        for key in result:
+            deleted = set()
+            for exp in query["nbr"]:
+                for its in result[key]:
+                    if its not in deleted:
+                        try:
+                            if not eval(exp):
+                                deleted.add(its)
+                        except AttributeError:
+                            deleted.add(its)
+            for obj in deleted:
+                result[key].remove(obj)
+        return result
+
+
 __relations__ = {
     (-1, -1, -1) : [6 , 53, 33], 
     (-1, -1,  0) : [7 , 30], 
@@ -45,10 +97,14 @@ __relations__ = {
     (1 ,  1,  1) : [17, 20, 36]
 }
 
+
 class Cube:
+
     """A cuboid based Rubik's Cube."""
+
     def __init__(self, colours=reduce(lambda x,y:x+y, map(lambda x:[x]*9, "LUFDRB"))):
-        self.cube_g = {}
+        global _CuboidsFilterDict
+        self.cube_g = _CuboidsFilterDict()
         for x in range(-1, 2):
             for y in range(-1, 2):
                 for z in range(-1, 2):
@@ -72,47 +128,53 @@ class Cube:
                         self.cube_g[key].add(_key)
                         self.cube_g[_key].add(key)
 
+
     def __getitem__(self, query):
+
         """
         This can filter the cube graph.
 
-        Cube[{ "key" : ["its.colour == 'R'"] , "nbr" : ["its.type == 'cuboid'"] }] =>
+        Make sure to use "its" keyword to specify "that key" or "that neighbor"
 
-        {Square(uid=36, colour='R'): {Cuboid(x=1, y=1, z=1)},
-         Square(uid=37, colour='R'): {Cuboid(x=1, y=1, z=0)},
-         Square(uid=38, colour='R'): {Cuboid(x=1, y=1, z=-1)},
-         Square(uid=39, colour='R'): {Cuboid(x=1, y=0, z=1)},
-         Square(uid=40, colour='R'): {Cuboid(x=1, y=0, z=0)},
-         Square(uid=41, colour='R'): {Cuboid(x=1, y=0, z=-1)},
-         Square(uid=42, colour='R'): {Cuboid(x=1, y=-1, z=1)},
-         Square(uid=43, colour='R'): {Cuboid(x=1, y=-1, z=0)},
+        EX:
+        Cube[{
+            "key" : [ " its.colour == 'R' " ], 
+            "nbr" : [ " its.type == 'cuboid' " ]
+        }] =>
+
+        {Square(uid=36, colour='R'): {Cuboid(x=1, y=1 , z=1 )},
+         Square(uid=37, colour='R'): {Cuboid(x=1, y=1 , z=0 )},
+         Square(uid=38, colour='R'): {Cuboid(x=1, y=1 , z=-1)},
+         Square(uid=39, colour='R'): {Cuboid(x=1, y=0 , z=1 )},
+         Square(uid=40, colour='R'): {Cuboid(x=1, y=0 , z=0 )},
+         Square(uid=41, colour='R'): {Cuboid(x=1, y=0 , z=-1)},
+         Square(uid=42, colour='R'): {Cuboid(x=1, y=-1, z=1 )},
+         Square(uid=43, colour='R'): {Cuboid(x=1, y=-1, z=0 )},
          Square(uid=44, colour='R'): {Cuboid(x=1, y=-1, z=-1)}}
         """
-        _query = {"key":[], "nbr":[]}
-        _query.update(query)
-        result = dict(zip(self.cube_g.keys(), map(lambda x:x.copy(), self.cube_g.values())))
-        deleted = set()
-        for exp in _query["key"]:
-            for its in self.cube_g:
-                if its not in deleted:
-                    try:
-                        if not eval(exp):
-                            deleted.add(its)
-                    except AttributeError:
-                        deleted.add(its)
-        for obj in deleted:
-            del result[obj]
-        for key in result:
-            deleted = set()
-            for exp in _query["nbr"]:
-                for its in result[key]:
-                    if its not in deleted:
-                        try:
-                            if not eval(exp):
-                                deleted.add(its)
-                        except AttributeError:
-                            deleted.add(its)
-            for obj in deleted:
-                result[key].remove(obj)
+
+        return self.cube_g.filter(**query)
+
+
+    def _rotate(self, exp, axis, clockwise=True, double=False):
+
+        """
+        ###
+        Don't use this.
+        ###
+        
+        Performing a rotation.
+        Rotate the cuboids, 
+        the cuboids which will be rotated: Cube[ { "key" : [ exp ] } ]
+        """
+
+        clockwise = clockwise * 2 - 1
+        clockwise = eval("{}1 * {}".format(axis[1], clockwise))
+        result = self[{}]
+        selected = self[{"key":[exp]}]
         return result
 
+
+if __name__ == '__main__':
+    a = Cube()
+    print(a._rotate("its.x == 1", ""))
