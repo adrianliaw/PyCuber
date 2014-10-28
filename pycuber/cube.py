3550c3663147df0389f2a304439ea08a0a2d1a26
adrianliaw <adrianliaw2000@gmail.com>
2014-10-28 22:31:21 +0800

    Finished

--- pycuber/cube.py
+++ pycuber/cube.py
@@ -1,5 +1,7 @@
 from .algorithm import *
+from .util import FrozenDict
 from functools import reduce
+from itertools import permutations
 
 class Square(object):
     """Square(colour), implements a square (sticker) on a cube."""
@@ -16,7 +18,7 @@ class Square(object):
                     "not {0}.".format(colour))
         self.colour = colour
         self.parent = parent
-        self.children = children
+        self.children = set(children)
 
     def __repr__(self):
         """
@@ -76,46 +78,417 @@ class Cuboid(object):
         super(Cuboid, self).__init__()
         for kw in kwargs:
             if kw not in list("LUFDRB"):
-                raise ValueError("Facings must be L U F D R B, not {0}.".format(kw))
+                raise ValueError(
+                    "Facings must be L U F D R B, not {0}."
+                    .format(kw), 
+                    )
             elif isinstance(kwargs[kw], str):
                 kwargs[kw] = Square(kwargs[kw])
-        self._facings = kwargs
+        self.facings = FrozenDict(kwargs)
         self.parent = parent
-        self.children = children
+        self.children = set(children)
+        self.location = "".join(kwargs)
 
     def __repr__(self):
         """
         Print out "Cuboid(U:\x1b[43m ...)"
         """
-        return "Cuboid(" + \
-            " , ".join([
-                "{0}:{1}".format(key, value)
-                for key, value in self._facings.items()
-                ]) + \
-            ")"
+        return "{0}({1})".format(
+            self.__class__.__name__, 
+            str(self.facings)[1:-1], 
+            )
 
     def __getitem__(self, face):
         """
         Cuboid["L"] => Returns the square that positioned at L face.
         """
-        return self._facings[face]
+        return self.facings[face]
 
     def __hash__(self):
         """
         Cuboid object is hashable.
         """
-        return reduce(lambda x, y:x + hash(y), self._facings.values())
+        return reduce(
+            lambda x, y: hash(x) + hash(y), 
+            self.facings.values(), 
+            list(self.facings.values())[0], 
+            ) // hash(self.__class__.__name__)
 
     def __eq__(self, another):
         """
         Check if two Cuboids are the same.
         """
-        return self._facings == another._facings
+        if isinstance(another, Cuboid):
+            return self.facings == another.facings
+        return False
 
     def __ne__(self, another):
         """
         Check if two Cuboids are different.
         """
         return not self.__eq__(another)
+
+    def __contains__(self, value):
+        """
+        Check if the Cuboid uses that face.
+        """
+        return value in self.facings
+
+    def __or__(self, value):
+        """
+        Check if the Cuboid uses that colour.
+        """
+        try:
+            return Square(value) in self.facings.values()
+        except ValueError:
+            return False
+
+    def __and__(self, another):
+        """
+        Check if two Cuboids have the same location.
+        """
+        if isinstance(another, str):
+            return tuple(self.facings) in permutations(another, len(another))
+        try:
+            return self.facings.viewkeys() == another.facings.viewkeys()
+        except AttributeError:
+            return self.facings.keys() == another.facings.keys()
+
+    def copy(self):
+        try:
+            new = {
+                "centre": Centre, 
+                "edge": Edge, 
+                "corner": Corner, 
+                }[self.type](
+                    parent=self.parent, 
+                    children=self.children, 
+                    **self.facings
+                    )
+        except AttributeError:
+            new = Cuboid(
+                parent=self.parent, 
+                children=self.children, 
+                **self.facings
+                )
+        return new
+
+
+class Centre(Cuboid):
+    """
+    Centre(U=Square("yellow")) => Implements the "Centre Block" (has 1 sticker).
+    """
+    def __init__(self, parent=None, children=[], **kwargs):
+        if len(kwargs) != 1:
+            raise ValueError("A Centre has 1 Square, got {0}.".format(len(kwargs)))
+        super(Centre, self).__init__(parent, children, **kwargs)
+        self.type = "centre"
+        self.colour = list(kwargs.values())[0].colour
+        self.face = list(kwargs.keys())[0]
+
+ 
+class Edge(Cuboid):
+    """
+    Edge(U=Square("yellow"), F=Square("green")) => Implements the "Edge Block" (has 2 stickers).
+    """
+    def __init__(self, parent=None, children=[], **kwargs):
+        if len(kwargs) != 2:
+            raise ValueError("An Edge has 2 Squares, got {0}.".format(len(kwargs)))
+        super(Edge, self).__init__(parent, children, **kwargs)
+        self.type = "edge"
+
+
+class Corner(Cuboid):
+    """
+    Corner(
+        U=Square("yellow"), 
+        F=Square("green"), 
+        R=Square("orange"), 
+        ) => Implements the "Corner Block" (has 3 stickers).
+    """
+    def __init__(self, parent=None, children=[], **kwargs):
+        if len(kwargs) != 3:
+            raise ValueError("A Corner has 3 Squares, got {0}.".format(len(kwargs)))
+        super(Corner, self).__init__(parent, children, **kwargs)
+        self.type = "corner"
+
+
+class Cube(object):
+    """
+    Cube([, {a set of Cuboids}]) => Implements a Rubik's Cube.
+    """
+    def __init__(self, cuboids=None):
+        super(Cube, self).__init__()
+        self.parent = None
+        self.children = set()
+        if not cuboids:
+            cuboids = set()
+            colours = {"L":"red", "U":"yellow", "F":"green", "D":"white", "R":"orange", "B":"blue"}
+            for loc in [
+                "LDB", "LDF", "LUB", "LUF", "RDB", "RDF", "RUB", "RUF", 
+                "LB", "LF", "LU", "LD", "DB", "DF", "UB", "UF", "RB", "RF", "RU", "RD", 
+                "L", "R", "U", "D", "F", "B", 
+                ]:
+                if len(loc) == 3:
+                    cuboids.add(Corner(**{loc[i]:Square(colours[loc[i]]) for i in range(3)}))
+                elif len(loc) == 2:
+                    cuboids.add(Edge(**{loc[i]:Square(colours[loc[i]]) for i in range(2)}))
+                else:
+                    cuboids.add(Centre(**{loc[0]:Square(colours[loc[0]])}))
+        if isinstance(cuboids, set):
+            for cubie in cuboids:
+                if isinstance(cubie, Cuboid):
+                    children = set()
+                    for sqr in cubie.facings.values():
+                        children.add(Square(sqr))
+                    if len(cubie.location) == 3:
+                        child_class = Corner
+                    elif len(cubie.location) == 2:
+                        child_class = Edge
+                    elif len(cubie.location) == 1:
+                        child_class = Centre
+                    self.children.add(child_class(parent=self, children=children, **cubie.facings))
+                else:
+                    raise ValueError("Should use Cuboid, not {0}.".format(cubie.__class__.__name__))
+
+    def __repr__(self):
+        """
+        Draw the Cube as expanded view.
+        """
+        result = ""
+        _ = {
+            "L": self.L, 
+            "U": self.U, 
+            "F": self.F, 
+            "D": self.D, 
+            "R": self.R, 
+            "B": self.B, 
+            }
+        for i in range(3):
+            result += "      " + "".join(str(square) for square in _["U"][i]) + "\n"
+        for i in range(3):
+            for side in "LFRB":
+                result += "".join(str(square) for square in _[side][i])
+            result += "\n"
+        for i in range(3):
+            result += "      " + "".join(str(square) for square in _["D"][i]) + "\n"
+        return result
+
+
+    def __getitem__(self, key):
+        """
+        Get specific Cuboid by location.
+        """
+        for child in self.children:
+            if child & key:
+                return child
+        raise KeyError(str(key))
+
+    def __setitem__(self, key, value):
+        """
+        Reset a specific Cuboid.
+        """
+        if self[key].type != value.type:
+            raise ValueError(
+                "Replacement of {0} must be {1}, not {2}."
+                .format(key, self[key].type, value.type), 
+                )
+        if not self[key] & value:
+            raise ValueError(
+                "Location must be {0}, not {1}."
+                .format(key, value.location), 
+                )
+        q = self[key]
+        self.children.remove(q)
+        q.facings = value.facings
+        q.children = set(value.facings.values())
+        self.children.add(q)
+
+    def __getattr__(self, name):
+        """
+        Returns the face from Cube.get_face() if the name is L U F D R or B.
+        """
+        if name in list("LUFDRB"):
+            return self.get_face(name)
+        else:
+            return super(Cube, self).__getattribute__(name)
+
+    def __call__(self, algo):
+        """
+        A shortcut for Cube.perform_algo().
+        """
+        return self.perform_algo(algo)
     
+    def __iter__(self):
+        """
+        Iterate over every Cuboid in the Cube.
+        """
+        result = []
+        for loc in [
+            "BDL", "FDL", "ULF", "ULB", "RUF", "RUB", "RDB", "RDF", 
+            "LU", "FU", "RU", "BU", "LF", "LB", "RF", "RB", "FD", "LD", "BD", "RD", 
+            "F", "U", "R", "L", "D", "B"
+            ]:
+            result.append((loc, self[loc]))
+        return iter(result)
+
+    def __eq__(self, another):
+        """
+        Check if two Cubes are the same.
+        """
+        return dict(self) == dict(another)
+
+    def __ne__(self, another):
+        """
+        Check if two Cubes aren't the same.
+        """
+        return not self.__eq__(another)
+
+    def at_face(self, face):
+        """
+        Find all Cuboids which have a Square on specific face.
+        """
+        return set(
+            child for child in self.children
+            if face in child.location
+            )
+
+    def has_colour(self, colour):
+        """
+        Find all Cuboids which has a specific colour(s).
+        """
+        return set(
+            child for child in self.children
+            if colour in map(
+                lambda x: x.colour, 
+                child.children, 
+                )
+            )
+
+    def select_type(self, tp):
+        """
+        Find all Cuboids which has the specific type.
+        """
+        return set(
+            child for child in self.children
+            if tp == child.type
+            )
+
+    def get_face(self, face):
+        """
+        Getting specific face on a Cube.
+        Returns as a 2D list.
+        """
+        if face not in [
+            "L", "U", "F", "D", "R", "B", 
+            "left", "up", "front", "down", "right", "back"
+            ]:
+            raise ValueError("Face must be L U F D R B, not {0}.".format(face))
+        elif face.islower():
+            face = face[0].upper()
+        result = [[None] * 3, [None] * 3, [None] * 3]
+        ordering = {
+            "L": "UDBF", 
+            "R": "UDFB", 
+            "U": "BFLR", 
+            "D": "FBLR", 
+            "F": "UDLR", 
+            "B": "UDRL", 
+            }[face]
+        for cuboid in self.at_face(face):
+            loc = [None, None]
+            for f in cuboid.facings:
+                if cuboid.type == "centre":
+                    loc = [1, 1]
+                if f != face:
+                    if cuboid.type == "edge":
+                        loc[ordering.index(f) // 2] = (ordering.index(f) % 2) * 2
+                        loc[loc.index(None)] = 1
+                    elif cuboid.type == "corner":
+                        loc[ordering.index(f) // 2] = (ordering.index(f) % 2) * 2
+            result[loc[0]][loc[1]] = cuboid.facings[face]
+        return result
+
+    def _single_layer(self, step):
+        """
+        Helper function for Cube.perform_step().
+        Perform single layer steps.
+        """
+        step = Step(step)
+        movement = {
+            "U": "RFLB", 
+            "D": "LFRB", 
+            "R": "FUBD", 
+            "L": "FDBU", 
+            "F": "URDL", 
+            "B": "ULDR", 
+            "M": ("LR", "FDBU"), 
+            "S": ("FB", "URDL"), 
+            "E": ("UD", "LFRB"), 
+            }[step.face]
+        if len(movement) == 2: slice_, movement = movement
+        if step.is_inverse: movement = movement[::-1]
+        if step.face not in "MSE":
+            to_move = {c.copy() for c in self.at_face(step.face)}
+        else:
+            to_move = {
+                c.copy()
+                for c in (self.children - self.at_face(slice_[0]) - self.at_face(slice_[1]))
+                }
+        for cuboid in to_move:
+            new = {}
+            for f in cuboid.facings:
+                if f != step.face:
+                    new[movement[(movement.index(f) + step.is_180 + 1) % 4]] = cuboid.facings[f]
+                else:
+                    new[f] = cuboid.facings[f]
+            new_cuboid = {
+                "centre": Centre, 
+                "edge": Edge, 
+                "corner": Corner, 
+                }[cuboid.type](
+                    parent=self, 
+                    children=new.values(), 
+                    **new 
+                    )
+            self[new_cuboid.location] = new_cuboid
+        return self
+
+    def _other_rotations(self, step):
+        """
+        Helper function for Cube.perform_step().
+        Perform wide rotations or cube rotations.
+        """
+        step = Step(step)
+        movement = {
+            "x": ["L'", "M'", "R"], 
+            "y": ["U", "E'", "D'"], 
+            "z": ["F", "S", "B'"], 
+            "r": ["R", "M'"], 
+            "l": ["L", "M"], 
+            "u": ["U", "E'"], 
+            "d": ["D", "E"], 
+            "f": ["F", "S"], 
+            "b": ["B", "S'"], 
+            }[step.face]
+        for s in movement:
+            step_ = Step(s)
+            if step.is_inverse: step_ = step_.inverse()
+            elif step.is_180: step_ = step_ * 2
+            self._single_layer(step_)
+        return self
+
+    def perform_step(self, step):
+        step = Step(step)
+        if step.face in "LUFDRBMES":
+            return self._single_layer(step)
+        else:
+            return self._other_rotations(step)
+
+    def perform_algo(self, algo):
+        algo = Algo(algo)
+        for step in algo:
+            self.perform_step(step)
+        return self
+
 
--- pycuber/util.py
+++ pycuber/util.py
@@ -63,3 +63,34 @@ def path_states(path):
 def path_actions(path):
     "Return a list of actions in this path."
     return path[1::2]
+
+class FrozenDict(dict):
+
+    __doc__ = dict.__doc__
+
+    def _delattr(func):
+        def EmptyAttribute(self, *args, **kwargs):
+            raise AttributeError(
+                "'FrozenDict' object has no attribute '{0}'"
+                .format(func.__name__)
+                )
+        return EmptyAttribute
+
+    @_delattr
+    def __setitem__(): pass
+    @_delattr
+    def __delitem__(): pass
+    @_delattr
+    def clear(): pass
+    @_delattr
+    def pop(): pass
+    @_delattr
+    def popitem(): pass
+    @_delattr
+    def setdefault(): pass
+    @_delattr
+    def update(): pass
+
+    del _delattr
+
+            
