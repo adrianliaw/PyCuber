0da88d7f5a38228462e1194a7ad89d2974f4bf76
adrianliaw <adrianliaw2000@gmail.com>
2014-11-20 20:35:52 +0800

    Fixed bug

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     name="pycuber", 
     version="0.1b2", 
     description="Rubik's Cube in Python",
-    long_description=read("README.md");
+    long_description=read("README.md"), 
     url="http://github.com/adrianliaw/PyCuber", 
     license="MIT", 
     author="Adrian Liaw", 
