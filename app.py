411cab34bcd9fe5bab5bf78a1b282be7292100d3
adrianliaw <adrianliaw2000@gmail.com>
2014-11-21 23:21:08 +0800

    restructured project

---
+++ app.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template
+import websockets
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("main.html")
+
+if __name__ == "__main__":
+    app.run()
