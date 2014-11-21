870a0bcb1cef8624277b9a6eabe885a30b669504
adrianliaw <adrianliaw2000@gmail.com>
2014-11-21 14:57:07 +0800

    delete tests

--- setup.py
+++ setup.py
@@ -1,23 +1,70 @@
 from setuptools import setup
-import os
 
-def read(*paths):
-    """Build a file path from *paths* and return the contents."""
-    with open(os.path.join(*paths), 'r') as f:
-        return f.read()
+long_desc = """
+PyCuber
+=======
+
+PyCuber is a Rubik’s Cube simulator in Python 2/3.
+--------------------------------------------------
+
+The cube can be revealed as expanded view in the terminal, so it’s easy
+to visualise the cube, just inside the terminal.
+
+.. code:: python
+
+
+    import pycuber as pc
+
+    # Create a Cube object
+    mycube = pc.Cube()
+
+    # Do something at the cube.
+    mycube("R U R' U'")
+
+    print(mycube)
+
+.. figure:: https://raw.github.com/adrianliaw/PyCuber/v0.1b/static/img/terminal.png
+   :alt: alt tag
+
+   alt tag
+We also provided some useful tools to deal with Rubik’s Cube algorithms.
+
+.. code:: python
+
+
+    import pycuber as pc
+
+    # Create an Algo object
+    myalg = pc.Algo("R U R' U' R' F R2 U' R' U' R U R' F'")
+
+    # Reversing an Algo
+    myalg.reverse()
+    print(myalg)
+
+    # Mirroring an Algo object
+    myalg.mirror("LR")
+    print(myalg)
+
+::
+
+    F R U' R' U R U R2 F' R U R U' R'
+    F' L' U L U' L' U' L2 F L' U' L' U L
+
+I’ll add some documentations later.
+"""
 
 setup(
-    name="pycuber", 
-    version="0.1b2", 
-    description="Rubik's Cube in Python",
-    long_description="See http://github.com/adrianliaw/PyCuber", 
-    url="http://github.com/adrianliaw/PyCuber", 
-    license="MIT", 
-    author="Adrian Liaw", 
-    author_email="adrianliaw2000@gmail.com", 
-    packages=["pycuber"], 
-    package_dir={"pycuber":"pycuber"}, 
-    classifiers=[
+    name = "pycuber", 
+    version = "0.1b3", 
+    description = "Rubik's Cube in Python",
+    long_description = long_desc, 
+    url = "http://github.com/adrianliaw/PyCuber", 
+    license = "MIT", 
+    author = "Adrian Liaw", 
+    author_email = "adrianliaw2000@gmail.com", 
+    packages = ["pycuber"], 
+    package_dir = {"pycuber":"pycuber"}, 
+    classifiers = [
         "Development Status :: 4 - Beta", 
         "Environment :: Console", 
         "Intended Audience :: Science/Research", 
