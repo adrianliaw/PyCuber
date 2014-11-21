bd962e7aa4c75e0e0ee39c4ad976b427a3b2c0d3
adrianliaw <adrianliaw2000@gmail.com>
2014-11-21 15:42:09 +0800

    PyPI readme

--- setup.py
+++ setup.py
@@ -10,46 +10,44 @@ PyCuber is a Rubik’s Cube simulator in Python 2/3.
 The cube can be revealed as expanded view in the terminal, so it’s easy
 to visualise the cube, just inside the terminal.
 
-.. code:: python
+.. code-block:: python
 
 
-    import pycuber as pc
+    >>> import pycuber as pc
 
-    # Create a Cube object
-    mycube = pc.Cube()
+    >>> # Create a Cube object
+    >>> mycube = pc.Cube()
 
-    # Do something at the cube.
-    mycube("R U R' U'")
+    >>> # Do something at the cube.
+    >>> mycube("R U R' U'")
 
-    print(mycube)
+    >>> print(mycube)
 
-::
+.. image:: https://camo.githubusercontent.com/906f83f4933fe1d0741b7d3ff43bda66fb464cdd/68747470733a2f2f7261772e6769746875622e636f6d2f61647269616e6c6961772f507943756265722f76302e31622f7374617469632f696d672f7465726d696e616c2e706e67
 
 We also provided some useful tools to deal with Rubik’s Cube algorithms.
 
-.. code:: python
+.. code-block:: python
 
 
-    import pycuber as pc
+    >>> import pycuber as pc
 
-    # Create an Algo object
-    myalg = pc.Algo("R U R' U' R' F R2 U' R' U' R U R' F'")
+    >>> # Create an Algo object
+    >>> myalg = pc.Algo("R U R' U' R' F R2 U' R' U' R U R' F'")
 
-    # Reversing an Algo
-    myalg.reverse()
-    print(myalg)
+    >>> # Reversing an Algo
+    >>> myalg.reverse()
+    >>> print(myalg)
 
-    # Mirroring an Algo object
-    myalg.mirror("LR")
-    print(myalg)
+    >>> # Mirroring an Algo object
+    >>> myalg.mirror("LR")
+    >>> print(myalg)
 
-::
 
     F R U' R' U R U R2 F' R U R U' R'
     F' L' U L U' L' U' L2 F L' U' L' U L
 
-I’ll add some documentations later.
-"""
+I’ll add some documentations later."""
 
 setup(
     name = "pycuber", 
