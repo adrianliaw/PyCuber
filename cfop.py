84774d4960a7f028dfac511afc097e19f80223c2
adrianliaw <adrianliaw2000@gmail.com>
2014-11-22 09:11:01 +0800

    test sockets

---
+++ cfop.py
@@ -0,0 +1,33 @@
+from pycuber.solver.cfop.cross import CrossSolver
+from pycuber.solver.cfop.f2l import F2LSolver
+from pycuber.solver.cfop.oll import OLLSolver
+from pycuber.solver.cfop.pll import PLLSolver
+import pycuber as pc
+
+class CFOPSolver(object):
+    """
+    Implement another solver that yields every step.
+    """
+    def __init__(self, cube):
+        self.cube = cube
+
+    def solve(self):
+        result = pc.Algo()
+        solver = CrossSolver(self.cube)
+        cross = solver.solve()
+        yield "cross", cross
+        result += cross
+        solver = F2LSolver(self.cube)
+        for pair in solver.solve():
+            f2l = pair[1]
+            yield "F2L {0}".format(pair[0]), f2l
+            result += f2l
+        solver = OLLSolver(self.cube)
+        oll = solver.solve()
+        yield "OLL", oll
+        result += oll
+        solver = PLLSolver(self.cube)
+        pll = solver.solve()
+        yield "PLL", pll
+        result += pll
+        return result
--- sockets.py
+++ sockets.py
@@ -1,2 +1,18 @@
 import asyncio
 import websockets
+import cfop
+import pycuber as pc
+
+@asyncio.coroutine
+def cfop_solve(websocket, path):
+    cube = yield from websocket.recv()
+    cube = pc.Cube(cube)
+    solver = cfop.CFOPSolver(cube)
+    for step in solver.solve():
+        yield from websocket.send(str(step))
+
+start_server = websockets.serve(cfop_solve, "127.0.0.1", 8765)
+
+asyncio.get_event_loop().run_until_complete(start_server)
+asyncio.get_event_loop().run_forever()
+
