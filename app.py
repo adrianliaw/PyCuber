4c19b6c98a3a4ed47d9553e4d0e4b4f80249a9a9
adrianliaw <adrianliaw2000@gmail.com>
2014-11-26 09:29:33 +0800

    init angular

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template
-from flask.ext.triangle import Triangle
+from flask_triangle import Triangle
 
 app = Flask(__name__)
 Triangle(app)
 
 @app.route("/")
 def index():
+    print(True)
     return render_template("main.html")
 
 if __name__ == "__main__":
---
+++ static/js/app.js
@@ -0,0 +1,11 @@
+(function () {
+
+    var app = angular.module("rubikApp", []);
+
+    app.controller("MainCtrl", function ($scope) {
+        
+        
+
+    });
+
+})();
