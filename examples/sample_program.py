4a298e3e4f1a708793304b1f318f3a66b447e5e9
adrianliaw <adrianliaw2000@gmail.com>
2016-02-22 10:15:06 +0800

    Use enumerating generator instead of range to avoid raising error when trying to solve a solved cube

--- examples/sample_program.py
+++ examples/sample_program.py
@@ -1,14 +1,12 @@
 import sys
 import os
-sys.path.insert(0, os.getcwd())
 import pycuber as pc
+from pycuber.solver import CFOPSolver
+
 c = pc.Cube()
 alg = pc.Formula()
 random_alg = alg.random()
 c(random_alg)
-#print(c)
-
-from pycuber.solver import CFOPSolver
 
 solver = CFOPSolver(c)
 
--- pycuber/solver/cfop/__init__.py
+++ pycuber/solver/cfop/__init__.py
@@ -1,8 +1,11 @@
-import sys, time, pycuber
+import sys
+import time
+import pycuber
+
 if sys.version_info > (3,4):
-    import io as generalized_io
+    import io
 else:
-    import cStringIO as generalized_io
+    import cStringIO as io
 
 from .cross import CrossSolver
 from .f2l import F2LSolver
@@ -17,42 +20,41 @@ class CFOPSolver(object):
     def feed(self, cube):
         self.cube = cube
 
-    def solve(self, suppress_progress_messages):
+    def solve(self, suppress_progress_messages=False):
         if suppress_progress_messages:
             save_stdout = sys.stdout
-            sys.stdout = generalized_io.StringIO()
+            sys.stdout = io.StringIO()
         if not self.cube.is_valid():
             raise ValueError("Invalid Cube.")
         result = pycuber.Formula()
         sys.stdout.write("Solver starts....")
+
         sys.stdout.write("\rSolving Cross ......")
         solver = CrossSolver(self.cube)
         cross = solver.solve()
         result += cross
-        sys.stdout.write("\rCross: {0}\n".format(cross))
+        sys.stdout.write("\x1b[2K\rCross: {0}\n".format(cross))
 
         solver = F2LSolver(self.cube)
-        try:
-            f2lall = solver.solve()
-            for i in range(4):
-                sys.stdout.write("\rSolving F2L#{0} ......".format(i))
-                f2l = next(f2lall)
-                result += f2l[1]
-                sys.stdout.write("\rF2L{0}: {1}\n".format(*f2l))
-        except StopIteration:
-            sys.stdout.write('\nCube might be already solved? \n')
+        for i, f2l_single in enumerate(solver.solve(), 1):
+            sys.stdout.write("Solving F2L#{0} ......".format(i))
+            result += f2l_single[1]
+            sys.stdout.write("\x1b[2K\rF2L{0}: {1}\n".format(*f2l_single))
+
         solver = OLLSolver(self.cube)
-        sys.stdout.write("\rSolving OLL ......")
+        sys.stdout.write("Solving OLL ......")
         oll = solver.solve()
         result += oll
-        sys.stdout.write("\rOLL:  {0}\n".format(oll))
+        sys.stdout.write("\x1b[2K\rOLL:  {0}\n".format(oll))
+
         solver = PLLSolver(self.cube)
         sys.stdout.write("\rSolving PLL ......")
         pll = solver.solve()
         result += pll
-        sys.stdout.write("\rPLL:  {0}\n".format(pll))
+        sys.stdout.write("\x1b[2K\rPLL:  {0}\n".format(pll))
+
         sys.stdout.write("\nFULL: {0}\n".format(result.optimise()))
+
         if suppress_progress_messages:
             sys.stdout = save_stdout
         return result
-
