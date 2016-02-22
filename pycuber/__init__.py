c8bb43b445f177fdb68684c02c52f6ec9e8fc008
adrianliaw <adrianliaw2000@gmail.com>
2016-02-22 10:51:33 +0800

    Bump to version 0.2.1

--- pycuber/__init__.py
+++ pycuber/__init__.py
@@ -3,6 +3,6 @@ from .helpers import fill_unknowns, array_to_cubies
 
 __all__ = ["Cube", "Cubie", "Centre", "Corner", "Edge", "Square", "Step", "Formula"]
 __title__ = "PyCuber"
-__version__ = "0.2.0"
+__version__ = "0.2.1"
 __author__ = "Adrian Liaw"
 __license__ = "MIT"
