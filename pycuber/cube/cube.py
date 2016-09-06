6d9c1aa688c40a66e7c322f8429683356ac89cd1
adrianliaw <adrianliaw2000@gmail.com>
2016-09-06 17:48:19 +0800

    Finish Cube class initialisation

--- pycuber/cube/cube.py
+++ pycuber/cube/cube.py
@@ -1,5 +1,30 @@
 import numpy as np
+from itertools import product
+from . import cubie
 
+
+U, L, F, R, B, D = range(6)
 
 class Cube(np.ndarray):
-    pass
+
+    def __new__(subtype, *args, **kwargs):
+        if len(args) > 0:
+            cube = np.array(args[0])
+            if cube.shape == (3, 3, 3, 6):
+                return cube.view(Cube)
+
+        cube = np.ndarray.__new__(subtype, (3, 3, 3, 6), "int8")
+        poses = zip(
+            product((L, None, R), (D, None, U), (F, None, B)),
+            product(*[range(3)]*3)
+        )
+        for faces, (x, y, z) in poses:
+            faces = [[f, f] for f in faces if f != None]
+            cube[x, y, z] = cubie.make_cubie(faces)
+
+        return cube
+
+
+if __name__ == "__main__":
+    cube = Cube()
+    print(cube)
