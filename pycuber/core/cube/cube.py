5e002605010aa92ead7f2c70b25e137f1fb07c43
adrianliaw <adrianliaw2000@gmail.com>
2016-09-16 18:13:04 +0800

    Create basic Cube class

--- pycuber/core/cube/cube.py
+++ pycuber/core/cube/cube.py
@@ -1,2 +1,17 @@
-class Cube(object):
-    pass
+from .cube_array import CubeArray
+from .cube_abc import CubeABC
+
+
+class Cube(CubeABC):
+
+    def __init__(self):
+        super().__init__()
+        self.__data = CubeArray()
+
+    def do_step(self, step):
+        pass
+
+    def do_formula(self, formula):
+        for step in formula:
+            self.do_step(step)
+        return self
