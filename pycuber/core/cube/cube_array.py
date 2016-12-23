182aa0073bb3dde7aabfe543a92270e7e39d4400
adrianliaw <adrianliaw2000@gmail.com>
2016-12-24 00:47:09 +0800

    Generalise CubeArray to cube with multiple layers

--- pycuber/core/cube/cube_array.py
+++ pycuber/core/cube/cube_array.py
@@ -1,23 +1,28 @@
 import numpy as np
 from itertools import product
-from colorama import Back
 from . import cubie_array as cubie
 from .constants import U, L, F, R, B, D, Y
 
 
 class CubeArray(np.ndarray):
 
-    def __new__(subtype, *args, **kwargs):
+    def __new__(subtype, *args, layers=3, **kwargs):
         if len(args) > 0:
             cube = np.array(args[0])
-            if cube.shape == (3, 3, 3, 6):
+            if cube.shape == (layers, layers, layers, 6):
                 return cube.view(CubeArray)
 
-        cube = np.ndarray.__new__(subtype, (3, 3, 3, 6), "int8")
+        cube = np.ndarray.__new__(subtype, (layers, layers, layers, 6), "int8")
+        cube.layers = layers
+
+        mid_layers = [None] * (layers - 2)
         poses = zip(
-            product((L, None, R), (D, None, U), (B, None, F)),
-            product(*[range(3)]*3)
+            product((L, *mid_layers, R),
+                    (D, *mid_layers, U),
+                    (B, *mid_layers, F)),
+            product(*[range(layers)] * 3)
         )
+
         for faces, (x, y, z) in poses:
             faces = [[f, f] for f in faces if f is not None]
             cube[x, y, z] = cubie.make_cubie(faces)
@@ -25,7 +30,7 @@ class CubeArray(np.ndarray):
         return cube
 
     def twist(self, axis, layer, k=1):
-        selector = [slice(0, 3), slice(0, 3), slice(0, 3)]
+        selector = [slice(0, self.layers)] * 3
         selector[axis] = layer
         self[selector] = cubie.rotate_on(axis, self[selector], k)
         if axis != Y:
@@ -33,24 +38,25 @@ class CubeArray(np.ndarray):
         self[selector] = np.rot90(self[selector], k)
 
     def get_face(self, face):
+        first, last = 0, self.layers - 1
         if face == U:
-            result = np.flipud(np.rot90(self[:, 2, :, U]))
+            result = np.flipud(np.rot90(self[:, last, :, U]))
         elif face == L:
-            result = np.rot90(np.transpose(self[0, :, :, L]))
+            result = np.rot90(np.transpose(self[first, :, :, L]))
         elif face == F:
-            result = np.rot90(self[:, :, 2, F])
+            result = np.rot90(self[:, :, last, F])
         elif face == R:
-            result = np.rot90(self[2, :, :, R], 2)
+            result = np.rot90(self[last, :, :, R], 2)
         elif face == B:
-            result = np.fliplr(np.rot90(self[:, :, 0, B]))
+            result = np.fliplr(np.rot90(self[:, :, first, B]))
         elif face == D:
-            result = np.rot90(self[:, 0, :, D])
+            result = np.rot90(self[:, first, :, D])
         return result.view(np.ndarray)
 
-    def get_face_colours(self):
-        return self[
-            [1, 0, 1, 2, 1, 1],
-            [2, 1, 1, 1, 1, 0],
-            [1, 1, 2, 1, 0, 1],
-            [U, L, F, R, B, D],
-        ].view(np.ndarray)
+    # def get_face_colours(self):
+    #     return self[
+    #         [1, 0, 1, 2, 1, 1],
+    #         [2, 1, 1, 1, 1, 0],
+    #         [1, 1, 2, 1, 0, 1],
+    #         [U, L, F, R, B, D],
+    #     ].view(np.ndarray)
