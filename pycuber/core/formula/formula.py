175a3117f5268091b146ead33724d7bff5c018f4
adrianliaw <adrianliaw2000@gmail.com>
2017-02-19 22:11:39 +0800

    Add BaseFormula init function and getitem, setitem

--- pycuber/core/formula/formula.py
+++ pycuber/core/formula/formula.py
@@ -9,6 +9,7 @@ SKIP = 0
 MOVE = 1
 FORMULA = 2
 
+
 def formula_method_wrapper(method_func, spec):
 
     @wraps(method_func)
@@ -16,13 +17,12 @@ def formula_method_wrapper(method_func, spec):
         inputs, output = spec
         class_mapping = {MOVE: self.__move__, FORMULA: self.__class__}
 
-        for tp in class_mapping:
-            if tp in inputs:
-                i = inputs.index(tp)
+        for i, tp in enumerate(inputs):
+            if tp in class_mapping:
                 if not isinstance(args[i], class_mapping[tp]):
                     args[i] = class_mapping[tp](args[i])
 
-        result = method_func(self, *args)
+        result = method_func(self._data, *args)
         if output in class_mapping:
             result = class_mapping[output](result)
         return result
@@ -36,7 +36,8 @@ class FormulaMeta(ABCMeta):
         base = bases[0]
         for method, spec in list(namespace.items()):
             if isinstance(spec, tuple):
-                if method in base.__abstractmethods__ or method not in dir(base):
+                if method in base.__abstractmethods__ or \
+                        method not in dir(base):
                     method_func = getattr(list, method)
                 else:
                     method_func = getattr(base, method)
@@ -54,10 +55,33 @@ class BaseFormula(MutableSequence, metaclass=FormulaMeta):
     insert = ([SKIP, MOVE], SKIP)
     extend = ([FORMULA], SKIP)
     copy = ([], FORMULA)
-    ### TODO: __init__, __setitem__, __getitem__, __reversed__, reverse
+
+    def __init__(self, formula):
+        if isinstance(formula, str):
+            formula = formula.split()
+        elif isinstance(formula, self.__move__):
+            formula = [formula]
+        self._data = [self.__move__(m) for m in formula]
+
+    def __getitem__(self, index):
+        item = self._data[index]
+        if isinstance(index, slice):
+            return self.__class__(item)
+        else:
+            return self.__move__(item)
+
+    def __setitem__(self, index, item):
+        if item is None:
+            del self._data[index]
+        elif isinstance(index, slice):
+            self._data[index] = self.__class__(item)
+        else:
+            self._data[index] = self.__move__(item)
+
 
 class GenericCubicFormula(BaseFormula):
     __move__ = GenericCubicMove
+
 
 class Formula(BaseFormula):
     __move__ = Move
